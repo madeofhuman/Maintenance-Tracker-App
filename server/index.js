630aeb0dd5054609bec922dec07cd5776de8358a
madeofhuman <josephodina@gmail.com>
2018-05-20 18:36:47 +0100

    Fix TravisCI mocha test failure

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 import http from 'http';
 
-const port = process.env.PORT || 1337;
+const port = (process.env.PORT || 1337);
 
-if (!module.parent) {
-  http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\nI am CJ Odina');
-  }).listen(port);
-}
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\nI am CJ Odina');
+}).listen(port);
 
-console.log(`Server running at http://127.0.0.1:${port}/`);
+console.log(`Server running on port ${port}/`);
