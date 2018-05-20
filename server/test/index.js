6c713efa7b3e216a82e2d6f798f02c989bf70d29
madeofhuman <josephodina@gmail.com>
2018-05-20 16:43:06 +0100

    Fix mocha permission error on TravisCI

--- server/test/index.js
+++ server/test/index.js
@@ -1,7 +1,7 @@
 import http from 'http';
 import assert from 'assert';
 
-import '../server/index';
+import '../test/index';
 
 describe('Example Node Server', () => {
   it('should return 200', (done) => {
