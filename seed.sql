424725236ec466c0010cde3f8f52f50d82070459
madeofhuman <josephodina@gmail.com>
2018-05-30 20:42:02 +0100

    chore(readme): update readme with installation instruction
    - add installation intruction to readme
    - update seed.sql
    
    [Finishes #157981510]

--- seed.sql
+++ seed.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS users(
   id serial,
-  first_name character varying(11),
-  last_name character varying(11),
-  email character varying(40),
-  role character varying(5),
-  password_hash character varying(250),
+  first_name character varying(11) NOT NULL,
+  last_name character varying(11) NOT NULL,
+  email character varying(40) NOT NULL,
+  role character varying(5) NOT NULL,
+  password_hash character varying(250) NOT NULL,
   created_at time with time zone,
   CONSTRAINT unique_data UNIQUE (email)
 );
@@ -12,12 +12,12 @@ CREATE TABLE IF NOT EXISTS users(
 
 CREATE TABLE IF NOT EXISTS requests(
   id serial,
-  type character varying(11),
-  item character varying(30),
+  type character varying(11) NOT NULL,
+  item character varying(30) NOT NULL,
   model character varying(30),
   detail text,
-  status character varying(11),
-  owner character varying(40),
+  status character varying(11) NOT NULL,
+  owner character varying(40) NOT NULL,
   created_at time with time zone,
   updated_at time with time zone,
   CONSTRAINT requests_pkey PRIMARY KEY (id)
