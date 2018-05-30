570643ed326984a4bee1db6568dd748a8a8c4bcd
madeofhuman <josephodina@gmail.com>
2018-05-30 19:57:11 +0100

    chore(env): define NODE_ENV with cross-env
    - Use cross-env to define NODE_ENV for production, dev, and test
    - Configure database connection to use different DBs for each NODE_ENV
    mode
    
    [Delivers #157954446]

--- server/database.js
+++ server/database.js
@@ -1,6 +1,12 @@
 import { Client } from 'pg';
 
-const config = process.env.DATABASE_URL;
+let config;
+
+if (process.env.NODE_ENV === 'test') {
+  config = process.env.DATABASE_URL_T;
+} else if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'dev') {
+  config = process.env.DATABASE_URL;
+}
 
 const client = new Client(config);
 client.connect();
