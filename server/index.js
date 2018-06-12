414c5f00876fefe915cbc62235073bfd5731a1ad
madeofhuman <josephodina@gmail.com>
2018-06-12 08:59:19 +0100

    bug(docs): fix Swagger not working on Heroku
    - fix for Swagger docs crashing app on Heroku
    
    [Fixes #158021032]

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import fs from 'fs';
 import path from 'path';
-import swaggerUi from 'swagger-ui-express';
+import swagger from 'swagger-ui-express';
 import swaggerDocument from './api-docs/swagger.json';
 
 import api1 from './routes/api1';
@@ -18,17 +18,14 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'),
 app.use(morgan('combined', { stream: accessLogStream }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/static', express.static(path.join(__dirname, '../UI/assets/')));
-
 app.use('/api/v1', api1);
-
 app.all('/api/v2*', (req, res) => {
   res.status(501).json({ message: 'We are working on creating a better experience for you' });
 });
-
+app.use('/docs', swagger.serve, swagger.setup(swaggerDocument));
 app.use('/', client);
-
 app.all('*', (req, res) => {
   res.status(404).json({ error: 'The resource you\'re looking for is not available' });
 });
